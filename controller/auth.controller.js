e585a7f2078514e7179efe4ca5b761e277cfb5eb
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-15 16:53:53 +0530

    removed unused var in entrie file

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,4 +1,7 @@
-export const signUp = async (req, res,next) => {}
+/* eslint-disable no-unused-vars */
+export const signUp = async (req, res,next) => {
+    return true;
+}
 
 export const signIn = async (req, res,next) => {}
 
