56090f5054e9b9ce1261ad3c9714effeea29343d
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-15 16:10:45 +0530

    created mongos db

---
+++ Database/mongodb.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import { DB_URI, NODE_ENV } from '../config/env.js'
+
+if(!DB_URI) {
+  throw new Error('DB_URI is not defined')
+}
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+    })
+    // eslint-disable-next-line no-undef
+    console.log(`MongoDB connected to ${NODE_ENV} successfully`)
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message)
+    // eslint-disable-next-line no-undef
+    process.exit(1)
+  }
+}
+
+export default connectDB;
