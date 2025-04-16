4fdf9eb952b20732c41a7c68f311ff7bafbd45fb
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-16 23:11:46 +0530

    auth routed to the code

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,10 +4,13 @@ import { signUp,signIn,signOut } from "../controller/auth.controller.js";
 
 const authRouter = Router();
 
-// path: /api/v1/auth/sign-up
 
+// -> /api/v1/auth/sign-up --> POST BODY --> {name, email, password} --> CREATES A NEW USER
 authRouter.post("/Sign-Up", signUp);
+
+// -> /api/v1/auth/sign-in --> POST BODY --> {email, password} --> VALIDATE AND CHECK-IN USER
 authRouter.post("/sign-in", signIn);
+
 authRouter.post("/sign-out", signOut);
 
 export default authRouter;
