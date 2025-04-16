2c355d1b2c08a257d29b483f358a8521a726da0a
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-16 09:57:18 +0530

    moving code to controller

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signUp,signIn,signOut } from "../controller/auth.controller";  
+import { signUp,signIn,signOut } from "../controller/auth.controller.js";  
 
 
 const authRouter = Router();
