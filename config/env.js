51f38660da87e2911e91422fa4f34327d45e9422
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-17 15:15:56 +0530

    added new keys to env

--- config/env.js
+++ config/env.js
@@ -5,8 +5,12 @@ config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
 export const { 
     PORT,
+    SERVER_URL,
     NODE_ENV,
     DB_URI,
     JWT_SECRET, JWT_EXPIRES_IN,
-    ARCJET_ENV,ARCJET_KEY
+    ARCJET_ENV,ARCJET_KEY,
+    ARCJET_API_KEY,ARCJET_API_URL,
+    QSTASH_URL,QSTASH_TOKEN,QSTASH_CURRENT_SIGNING_KEY,QSTASH_NEXT_SIGNING_KEY
+    
 } = process.env;
