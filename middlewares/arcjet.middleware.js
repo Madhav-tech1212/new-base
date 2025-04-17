f35d1a45a984f581bad0a9516e46c4d04a7e64f6
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-17 09:38:38 +0530

    comfigered to the app requirements

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,4 +1,22 @@
-import aj from '../config/arcjet.js'
-import User from "../models/user.model.js";
+import aj from "../config/arcjet.js";
+// import User from "../models/user.model.js";
 
-const arcjet = new User({})
+const arcjetMiddleware = async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req,{requested: 1});
+
+        if (decision.isDenied()) {
+            if(decision.reason.isRateLimit()) return res.status(429).json({error:"Rate Limit"});
+            if(decision.reason.isBot()) return res.status(403).json({error:"Bot detected"});
+
+            return res.status(403).json({error:"Access Denied"});
+
+        }
+        next();
+    }catch(error) {
+        console.log(`Arcjet Middleware error occured: ${error}`);
+        next(error);
+    }
+}
+
+export default arcjetMiddleware;
