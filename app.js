2d1640f293a4e0e16f0cddd9b54100e3af561698
madhav-k <madhavkarthickk.1212@gmail.com>
2025-04-17 09:38:53 +0530

    connected arcjet to app

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectDB from './Database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'; // Arcjet Middleware
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
+app.use(arcjetMiddleware); // Arcjet Middleware
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
